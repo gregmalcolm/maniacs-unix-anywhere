3253fb5f3d2d7c4903166a897ae8526bc2167a04
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-06 23:16:46 -0400

    Too many keywords

--- code.rb
+++ code.rb
@@ -1,11 +1,13 @@
 @find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
-@unix_keywords = @find_unix_keywords.select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
+def unix_keywords do
+  @find_unix_keywords
+  .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
-Object.ancestors
-Kernel.singleton_methods
-
-[:ps, :ls, :pwd].each do |cmd|
-  Kernel.define_method(cmd) do
+unix_keywords.each do |cmd|
+  Kernel.define_method(cmd) do |*args|
     puts "I can do #{cmd}"
   end
 end
+
+# Kernel.respond_to?(:eval)
+# Kernel.respond_to?(:printf)
