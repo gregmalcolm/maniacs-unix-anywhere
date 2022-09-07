7c42b7a8589edc52b9f1f164fd879a81ac7e360e
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-06 23:22:54 -0400

    Safeties are on

--- code.rb
+++ code.rb
@@ -1,13 +1,19 @@
 @find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
-def unix_keywords do
+
+def filtered_unix_keywords
   @find_unix_keywords
   .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
+  .reject { |cmd| Kernel.respond_to?(cmd) }
+end
 
-unix_keywords.each do |cmd|
+@unix_keywords = filtered_unix_keywords
+
+@unix_keywords.each do |cmd|
   Kernel.define_method(cmd) do |*args|
     puts "I can do #{cmd}"
   end
 end
 
-# Kernel.respond_to?(:eval)
-# Kernel.respond_to?(:printf)
+# ps
+# ls
+# printf
