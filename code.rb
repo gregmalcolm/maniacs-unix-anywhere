e45e29a25ff1577548996437bfaa336d6265fbd9
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-06 22:48:44 -0400

    Calamity

--- code.rb
+++ code.rb
@@ -1,9 +1,8 @@
-def find_unix_keywords
-  `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
-end
+@find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
+@unix_keywords = @find_unix_keywords.select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
-def unix_keywords
-  find_unix_keywords.select { |cmd| keyword.to_s =~ /^[a-z_]+$/ }
+def method_missing(name, *args)
+  if @unix_keywords.include?(name)
+    system name
+  end
 end
-
-#find_unix_keywords.grep(/\[/)
