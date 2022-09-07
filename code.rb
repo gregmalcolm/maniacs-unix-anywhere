4aeb43cfa6e72ea364f24da85784a95933e7ff47
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-06 22:12:59 -0400

    only method names allowed

--- code.rb
+++ code.rb
@@ -1,3 +1,10 @@
 def find_unix_keywords
   `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
 end
+
+def unix_keywords
+  find_unix_keywords
+  .select { |cmd| keyword.to_s =~ /^[a-z_]+$/ }
+end
+
+#find_unix_keywords.grep(/\[/)
