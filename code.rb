319d3b92a34ed4ed020a79b176f91d7620067e26
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-06 22:54:05 -0400

    Everything is an Object

--- code.rb
+++ code.rb
@@ -1,8 +1,18 @@
 @find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
 @unix_keywords = @find_unix_keywords.select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
+# def method_missing(name, *args)
+#   if @unix_keywords.include?(name)
+#     system name
+#   end
+# end
+
+def hello_is_real
+  "hi"
+end
+Object.instance_methods.grep(/hello/)
+
 def method_missing(name, *args)
-  if @unix_keywords.include?(name)
-    system name
-  end
+  puts name
+  super
 end
