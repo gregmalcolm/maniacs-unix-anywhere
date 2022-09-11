2c61d06d81a25e8fb28b55425913a2518d6eb269
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-11 16:08:55 -0400

    Tweak

--- code.rb
+++ code.rb
@@ -1,17 +1,14 @@
 @find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
 @unix_keywords = @find_unix_keywords.select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
-# def method_missing(name, *args)
-#   if @unix_keywords.include?(name)
-#     system name
-#   end
+# def spam(stuff)
+#   msg = stuff.to_s.upcase
+#   msg.size.times { puts stuff }
+#   msg
 # end
 
-def hi
-  "Hello is real!"
-end
-Object.instance_methods
-Object.instance_methods.grep(/hello/)
+# Object.instance_methods
+# Object.instance_methods.grep(/spam/)
 
 def method_missing(name, *args)
   puts name
