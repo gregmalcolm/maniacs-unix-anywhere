143fb50d296c35417c604361a17b7225f4ec4fe6
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-11 18:16:16 -0400

    Simplify

--- code.rb
+++ code.rb
@@ -1,6 +1,3 @@
-@find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
-@unix_keywords = @find_unix_keywords.select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
-
 def method_missing(name, *args)
   puts name
   super
