dc9f68dd13e706b1f06eaec463f78a8cef03bd7d
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-11 17:19:17 -0400

    untweak

--- code.rb
+++ code.rb
@@ -1,6 +1,4 @@
 @find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
 @unix_keywords = @find_unix_keywords.select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
-# `ps`
-# `compgen -abck`
 # @find_unix_keywords.grep(/\[/)
