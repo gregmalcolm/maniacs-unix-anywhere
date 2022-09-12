407b643e108a0d614fa90c774905ea87d3651bb8
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-11 22:45:49 -0400

    simplify

--- code.rb
+++ code.rb
@@ -1,4 +1,4 @@
 @find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
 @unix_keywords = @find_unix_keywords.select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
-# @find_unix_keywords.grep(/\[/)
+# `echo $(compgen -abck)`.split.uniq.map.grep(/\[/)
