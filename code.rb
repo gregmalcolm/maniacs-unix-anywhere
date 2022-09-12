058002ef76da6a0a127089d6732cff21062e634e
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-11 23:03:25 -0400

    Refine

--- code.rb
+++ code.rb
@@ -1,12 +1,10 @@
-@find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
-
-def filtered_unix_keywords
-  @find_unix_keywords
+@find_unix_keywords = \
+  `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
   .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
-  .reject { |cmd| Kernel.respond_to?(cmd) }
-end
 
-@unix_keywords = filtered_unix_keywords
+@unix_keywords = @find_unix_keywords.reject do |cmd|
+                   Kernel.respond_to?(cmd)
+                 end
 
 @unix_keywords.each do |cmd|
   Kernel.define_method(cmd) do |*args|
