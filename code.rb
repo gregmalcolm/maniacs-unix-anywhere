bea5ff3eb0039451a548e3fadede19cfbf79fbc5
Greg Malcolm <greg.malcolm@beam.dental>
2022-09-11 22:50:03 -0400

    simplify

--- code.rb
+++ code.rb
@@ -1,5 +1,6 @@
-@find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym)
-@unix_keywords = @find_unix_keywords.select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
+@find_unix_keywords = \
+  `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
+  .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
 def method_missing(name, *args)
   if @unix_keywords.include?(name)
