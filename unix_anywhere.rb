1cc65375bb19c96ee1fa4b66005c41abc3f6edb4
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 18:24:25 -0400

    Tidy up

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -1,12 +1,10 @@
-@unix_keywords = \
-  `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
-  .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
+#
+# Experiment: define methods on the Kernel
+#
 
+@unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 @unix_keywords.each do |cmd|
   Kernel.define_method(cmd) do |*args|
     puts "I can do #{cmd}"
   end
 end
-
-# Kernel.respond_to?(:eval)
-# Kernel.respond_to?(:printf)
