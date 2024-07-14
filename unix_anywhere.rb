e966f6d11432c89a38442bd9403ed922bde75b2b
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 18:20:00 -0400

    Kernel info

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -1,26 +1,21 @@
 #
-# Other experiment: Just unix keywords
+# Peeking into the Object
 #
 
 @unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
-def method_missing(name, *args)
-  #puts name
-  #puts name unless name.include?(%w[to_ary])
-  puts name name.include?(@unix_keywords)
-  super
-end
-
-# def spam
-#   msg = self.to_s.upcase
-#   msg.size.times { puts msg }
-#   msg
+# def method_missing(name, *args)
+#   #puts name
+#   #puts name unless name.include?(%w[to_ary])
+#   puts name name.include?(@unix_keywords)
+#   super
 # end
 
-# # Nowadays (Ruby 3.3) it's only accessibly privately
-# "Buy spam".send(:spam)
-# 42.send(:spam)
+# Object.ancestors
+# BasicObject.instance_methods # Introduced in Ruby 1.9
 
-# self.class.ancestors
-# puts "I'M ON A KERNEL"
-# Kernel.singleton_methods
+# Kernel.instance_methods
+# Kernel.instance_methods.count
+
+# BasicObject.respond_to?(:puts)
+# Kernel.respond_to?(:puts)
