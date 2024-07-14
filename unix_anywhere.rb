97ce8bc9f054889a123950f65d8c1911793edc44
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-13 23:37:24 -0400

    Drop find

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -1,4 +1,4 @@
-@find_unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
-                      .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
+@unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
+                 .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
 # `echo $(compgen -abck)`.split.uniq.map.grep(/\[/)
