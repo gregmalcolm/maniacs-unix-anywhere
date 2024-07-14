b815096ae548e8c051838b748bdb6f5f1aeb733f
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 17:25:26 -0400

    Nope to to_ary

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -1,11 +1,11 @@
 #
-# Experimet: Echo all the things
+# That was scary. to_ary gets a pass
 #
 
 @unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
 def method_missing(name, *args)
-  puts name
+  puts name unless name.include?(%w[to_ary])
   super
 end
 
