eba047dc540208363a36eec40170d5f77c5d99b9
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 17:06:11 -0400

    Object

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -1,11 +1,11 @@
 #
-# Adding a simple spam message to irb
+# All the world is an Object
 #
 
 @unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
 def spam
-  msg = "CRB"
+  msg = self.to_s
   msg.size.times do 
     puts "Hello #{msg}"
   end
@@ -13,6 +13,18 @@ def spam
 end
 
 #spam
+
+self
+self.class
+"CRB".class
+"CRB".class.superclass
+[].class
+[].class.superclass
+42.class
+42.class.superclass
+42.class.superclass.superclass
+
+
 
 # "Buy spam".send(:spam)
 # 42.send(:spam)
