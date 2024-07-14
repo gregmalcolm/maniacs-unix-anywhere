27d883814a2bcec70d2c483fd91fd2567d9846bf
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 16:33:42 -0400

    Cleanup

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -1,5 +1,18 @@
-#puts `echo $(compgen -abck)`.split.uniq.map.grep(/\[/)
+#
+# Which unix commands do we want to allow?
+#
+
+
+# puts `echo $(compgen -abck)`
+#   .split
+#   .uniq
+#   .map(&:to_sym)
+#   .grep(/^[^a-z]/)
 
 # @unix_keywords = \
-#   `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
+#   `echo $(compgen -abck)`
+#   .split
+#   .uniq
+#   .map(&:to_sym)
 #   .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
+# @unix_keywords
