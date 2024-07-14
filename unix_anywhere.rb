d4ef9946a6126da67d0ba3277917c40de905a207
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 18:00:58 -0400

    Cleanup

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -1,5 +1,5 @@
 #
-# Experimet: Echo all the things
+# Experiment: Echo all the things
 #
 
 @unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
@@ -8,17 +8,3 @@ def method_missing(name, *args)
   puts name
   super
 end
-
-# def spam
-#   msg = self.to_s.upcase
-#   msg.size.times { puts msg }
-#   msg
-# end
-
-# # Nowadays (Ruby 3.3) it's only accessibly privately
-# "Buy spam".send(:spam)
-# 42.send(:spam)
-
-# self.class.ancestors
-# puts "I'M ON A KERNEL"
-# Kernel.singleton_methods
