78f1f8fde34fbb6b740605675f542e62de710d8f
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 18:22:12 -0400

    One more method missing

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -4,13 +4,6 @@
 
 @unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
-# def method_missing(name, *args)
-#   #puts name
-#   #puts name unless name.include?(%w[to_ary])
-#   puts name name.include?(@unix_keywords)
-#   super
-# end
-
 # Object.ancestors
 # BasicObject.instance_methods # Introduced in Ruby 1.9
 
@@ -19,3 +12,12 @@
 
 # BasicObject.respond_to?(:puts)
 # Kernel.respond_to?(:puts)
+
+# class Kernel
+#   def method_missing(name, *args)
+#     #puts name
+#     #puts name unless name.include?(%w[to_ary])
+#     puts name name.include?(@unix_keywords)
+#     super
+#   end
+# end
