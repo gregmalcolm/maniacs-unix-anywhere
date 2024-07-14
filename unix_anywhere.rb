4455cbc259e6e83146938abb736ec0358c9133b4
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 16:38:47 -0400

    Cleaning up

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -1,6 +1,8 @@
-@unix_keywords = \
-  `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
-  .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
+#
+# Make the IRB understand unix commands
+#
+
+@unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
 # def method_missing(name, *args)
 #   return puts name if @unix_keywords.include?(name)
