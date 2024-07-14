bd3eb6475b2f4a0b13a2fde94d3e01bbe3ef7297
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 16:11:53 -0400

    Prepped

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -2,7 +2,7 @@
   `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
   .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
-def method_missing(name, *args)
-  return puts name if @unix_keywords.include?(name)
-  super
-end
+# def method_missing(name, *args)
+#   return puts name if @unix_keywords.include?(name)
+#   super
+# end
