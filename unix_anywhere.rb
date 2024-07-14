6bd93263dfbe99b57230c633eac11d1341d9c232
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-13 23:39:23 -0400

    Drop find

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -1,10 +1,11 @@
-@find_unix_keywords = \
+@unix_keywords = \
   `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
   .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
-@unix_keywords = @find_unix_keywords.reject do |cmd|
-                   Kernel.respond_to?(cmd)
-                 end
+@filtered_unix_keywords = \
+  @unix_keywords.reject do |cmd|
+    Kernel.respond_to?(cmd)
+  end
 
 @unix_keywords.each do |cmd|
   Kernel.define_method(cmd) do |*args|
