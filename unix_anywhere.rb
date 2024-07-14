bed1bb5d5c3cb58e86c5a0be8a101c61ae5e812e
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 18:41:50 -0400

    Newer layout also

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -2,16 +2,13 @@
 # Support for params. Sort of
 #
 
-@unix_keywords = \
-  `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
-  .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
-
-@filtered_unix_keywords = \
+@unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
+@allowed_unix_keywords = \
   @unix_keywords.reject do |cmd|
     Kernel.respond_to?(cmd)
   end
 
-@filtered_unix_keywords.each do |cmd|
+@allowed_unix_keywords.each do |cmd|
   Kernel.define_method(cmd) do |*args|
     system "#{cmd} #{args.join(' ')}"
   end
