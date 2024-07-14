363b79c8bd676c878448f9274eded9766cce4bd7
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 18:41:32 -0400

    Newer layout

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -2,16 +2,13 @@
 # Experiment: Remove support for unix commands tha clash with Kernel methods
 #
 
-@unix_keywords = \
-  `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
-  .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
-
-@filtered_unix_keywords = \
+@unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
+@allowed_unix_keywords = \
   @unix_keywords.reject do |cmd|
     Kernel.respond_to?(cmd)
   end
 
-@filtered_unix_keywords.each do |cmd|
+@allowed_unix_keywords.each do |cmd|
   Kernel.define_method(cmd) do |*args|
     system cmd.to_s
   end
