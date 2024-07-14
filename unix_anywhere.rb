995c8f99d334da628304c2d8fff64d5feb0d22ea
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 18:03:41 -0400

    Better transition

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -5,6 +5,8 @@
 @unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
 def method_missing(name, *args)
+  #puts name
+  #puts name unless name.include?(%w[to_ary])
   puts name name.include?(@unix_keywords)
   super
 end
