34b616ff4e0e023a6ec845ee571d7764d294a64e
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-13 23:40:10 -0400

    More readable

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -1,4 +1,5 @@
-@unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
-                 .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
+@unix_keywords = \
+  `echo $(compgen -abck)`.split.uniq.map(&:to_sym) \
+  .select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
 # `echo $(compgen -abck)`.split.uniq.map.grep(/\[/)
