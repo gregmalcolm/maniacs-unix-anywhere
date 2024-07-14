c778396a9dfb4d01f2727936ae445937444630d9
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 17:54:44 -0400

    Cleanup

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -9,32 +9,3 @@ def greet
   puts "Hello #{recipient}"
   recipient
 end
-
-#greet
-
-# "Buy spam".send(:spam)
-# 42.send(:spam)
-
-# def method_missing(name, *args)
-#   return puts name if @unix_keywords.include?(name)
-#   super
-# end
-
-# def method_missing(name, *args)
-#   puts name unless name.include?(%w[to_ary])
-#   super
-# end
-
-# def spam
-#   msg = self.to_s.upcase
-#   msg.size.times { puts msg }
-#   msg
-# end
-
-# # Nowadays (Ruby 3.3) it's only accessibly privately
-# "Buy spam".send(:spam)
-# 42.send(:spam)
-
-# self.class.ancestors
-# puts "I'M ON A KERNEL"
-# Kernel.singleton_methods
