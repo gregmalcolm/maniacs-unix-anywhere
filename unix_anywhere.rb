89610ae58f6489472c3ba0bd9d9d4ab5ad42c81e
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 23:02:41 -0400

    Spammy

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -4,18 +4,17 @@
 
 @unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 
-class Object
-  def spam
-    msg = self.to_s
-    msg.size.times do 
-        puts "Hello #{msg}"
-    end
-    msg
+def spam
+  msg = self.to_s
+  msg.size.times do 
+      puts "Hello #{msg}"
   end
+  msg
 end
 
 #spam
 
 #42.spam
 #42.send(:spam)
-#"Buy spam".send(:spam)
+#"buy spam".send(:spam)
+#But what if we wrap reopen Object and add spam there?
