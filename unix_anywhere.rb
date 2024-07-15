cecfcb5acef25c7eb09f20e7daed6cdc4d2c2552
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-14 23:26:03 -0400

    Better examples

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -3,11 +3,12 @@
 #
 
 @unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
-# @unix_keywords.each do |cmd|
-#   Kernel.define_method(cmd) do |*args|
-#     puts "I can do #{cmd}"
-#   end
-# end
+@unix_keywords.each do |cmd|
+  Kernel.define_method(cmd) do |*args|
+     puts "I can do #{cmd}"
+  end
+end
 
 # Kernel.respond_to?(:eval)
+# Kernel.respond_to?(:hash)
 # Kernel.respond_to?(:printf)
