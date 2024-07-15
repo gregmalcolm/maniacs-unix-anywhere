38feece74d47eef1d629436ff637b643f58175d6
Greg Malcolm <greg.malcolm@beam.dental>
2024-07-15 13:46:03 -0400

    I can do anything

--- unix_anywhere.rb
+++ unix_anywhere.rb
@@ -5,7 +5,7 @@
 @unix_keywords = `echo $(compgen -abck)`.split.uniq.map(&:to_sym).select { |cmd| cmd.to_s =~ /^[a-z_]+$/ }
 @unix_keywords.each do |cmd|
   Kernel.define_method(cmd) do |*args|
-     puts "I can do #{cmd}"
+    puts "I can do #{cmd} better than you"
   end
 end
 
